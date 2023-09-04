063060204f13f1eae1eadb68dcb1cf490e16bad1
Chris Milazzo <Christophermilazzo@hotmail.com>
2023-09-04 16:47:53 -0400

    D27: send email with news title and desc

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
+from send_email import send_email
 
 api_key = "18b05ed1f9f1448e92946cc428595f88/"
 url = ("https://newsapi.org/v2/top-headlines?sources="
-      "techcrunch&apiKey=18b05ed1f9f1448e92946cc428595f88")
+       "techcrunch&apiKey=18b05ed1f9f1448e92946cc428595f88")
 
 # Make request
 request = requests.get(url)
@@ -10,7 +11,10 @@ request = requests.get(url)
 # Get dictionary with data
 content = request.json()
 
-# Access content title and decription
-for articles in content["articles"]:
-    print(f"Title: {articles['title']}")
-    print(f"Desc: {articles['description']}")
+body = ""
+# Access content title and description and send email
+for article in content["articles"]:
+    body = body + article['title'] + "\n" + article['description'] + 2*'\n'
+
+body = body.encode("utf-8")
+send_email(message=body)
